8431a57850ef844f0f7ba06670f56b59a233105d
HyKhuong <144362203+HyKhuong@users.noreply.github.com>
2025-04-15 02:38:35 +0700

    Update tours_routes.php

--- server/routes/tours_routes.php
+++ server/routes/tours_routes.php
@@ -1,12 +1,1 @@
-<?php
-// routes.php
 
-$routes = [
-    ['GET', '/tours', 'TourController@index'],
-    ['GET', '/tours/{id}', 'TourController@get'],
-    ['POST', '/tours', 'TourController@create'],
-    ['PUT', '/tours/{id}', 'TourController@update'],
-    ['DELETE', '/tours/{id}', 'TourController@delete'],
-];
-
-return $routes;
